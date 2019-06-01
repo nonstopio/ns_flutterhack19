cd262a327b5b43df62930832aa55cd1957850da0
Ajay Kumar <ajay.kumar@nonstopio.com>
2019-06-01 10:38:28 +0530

    Added DemoScreen

---
+++ lib/screens/Zdemo/demo_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutterhackathon/Components/app_loading_widget.dart';
+import 'package:flutterhackathon/Utils/utils.dart';
+import 'package:flutterhackathon/components/app_error_widget.dart';
+
+class DemoScreen extends StatefulWidget {
+  @override
+  _DemoScreenState createState() => _DemoScreenState();
+}
+
+class _DemoScreenState extends State<DemoScreen> {
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalObjectKey<ScaffoldState>('DemoScreen');
+
+  PageState _pageState = PageState.Loading;
+  String message = "";
+
+  @override
+  void initState() {
+    super.initState();
+    appLogs("DemoScreen", tag: "Screen");
+    Future.delayed(Duration(milliseconds: 500), () => getDemoScreenDetails());
+  }
+
+  getDemoScreenDetails() async {
+    hideLoading();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          getBody(),
+          getLoaderWidget(),
+          getErrorWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget getBody() {
+    return Placeholder();
+  }
+
+  Widget getLoaderWidget() {
+    return Offstage(
+      offstage: _pageState != PageState.Loading,
+      child: FullScreenLoader(),
+    );
+  }
+
+  Widget getErrorWidget() {
+    return Offstage(
+      offstage: _pageState != PageState.Error,
+      child: AppErrorWidget(
+        message: message,
+        callback: () => getDemoScreenDetails(),
+      ),
+    );
+  }
+
+  showLoading() {
+    appLogs("DemoScreen:showLoading");
+
+    setState(() => _pageState = PageState.Loading);
+  }
+
+  hideLoading() {
+    appLogs("DemoScreen:hideLoading");
+
+    setState(() => _pageState = PageState.Loaded);
+  }
+
+  showError() {
+    appLogs("DemoScreen:showError");
+
+    setState(() => _pageState = PageState.Error);
+  }
+}
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutterhackathon/Components/app_loading_widget.dart';
+import 'package:flutterhackathon/Utils/app_constants.dart';
+import 'package:flutterhackathon/Utils/app_methods.dart';
+import 'package:flutterhackathon/components/app_error_widget.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalObjectKey<ScaffoldState>('LoginScreen');
+
+  PageState _pageState = PageState.Loading;
+  String message = "";
+
+  @override
+  void initState() {
+    super.initState();
+    appLogs("LoginScreen", tag: "Screen");
+    Future.delayed(Duration(milliseconds: 100), () => getLoginScreenDetails());
+  }
+
+  getLoginScreenDetails() async {
+    hideLoading();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          getBody(),
+          getLoaderWidget(),
+          getErrorWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget getBody() {
+    return Placeholder();
+  }
+
+  Widget getLoaderWidget() {
+    return Offstage(
+      offstage: _pageState != PageState.Loading,
+      child: FullScreenLoader(),
+    );
+  }
+
+  Widget getErrorWidget() {
+    return Offstage(
+      offstage: _pageState != PageState.Error,
+      child: AppErrorWidget(
+        message: message,
+        callback: () => getLoginScreenDetails(),
+      ),
+    );
+  }
+
+  showLoading() {
+    appLogs("LoginScreen:showLoading");
+
+    setState(() => _pageState = PageState.Loading);
+  }
+
+  hideLoading() {
+    appLogs("LoginScreen:hideLoading");
+
+    setState(() => _pageState = PageState.Loaded);
+  }
+
+  showError() {
+    appLogs("LoginScreen:showError");
+
+    setState(() => _pageState = PageState.Error);
+  }
+}
